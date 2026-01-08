5ba0491d577cff0dfda5edd3ebee556d2c9e1c30
select <falko.krause@protonmail.com>
2026-01-08 22:26:37 +0100

    feat: nicer splash screen

--- server/plugins/splash-screen.ts
+++ server/plugins/splash-screen.ts
@@ -1,13 +1,24 @@
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+
 /**
  * Nitro plugin to inject splash screen HTML into rendered pages.
  * This prevents FOUC (Flash of Unstyled Content) during hydration.
  */
 export default defineNitroPlugin(nitroApp => {
+  // Read the logo SVG file at build time
+  const logoPath = resolve(process.cwd(), 'app/assets/logo.svg')
+  const logoSvg = readFileSync(logoPath, 'utf-8')
+
   nitroApp.hooks.hook('render:html', html => {
     // Inject splash screen as first child of body
     html.bodyAppend.unshift(`
       <div id="app-splash">
-        <div class="splash-spinner"></div>
+        <div class="splash-content">
+          <div class="splash-logo">
+            ${logoSvg}
+          </div>
+        </div>
       </div>
     `)
   })
